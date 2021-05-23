ef52018f1204ad8abc14409ea9f4888451914c31
Egor <egorchirkov03@gmail.com>
2021-05-23 15:10:15 +0300

    Initial Commit

---
+++ TestLanscape/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestLanscape
+//
+//  Created by Mac mini on 18.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
